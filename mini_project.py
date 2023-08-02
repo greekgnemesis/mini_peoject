9f5416f023082a7868a3530db3947c43fb9b640a
greekgnemesis <124674265+greekgnemesis@users.noreply.github.com>
2023-08-02 11:58:07 +0400

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -1,7 +1,5 @@
 import random
-import sys
  
-    
 start = input(" TO START THE GAME TYPE 'yes' TO QUIT TYPE 'no'/n ")
 if start.lower() == "no":
     sys.exit()
@@ -10,14 +8,14 @@ else:
 
 a = input("welcome to the game of chance,are you ready to test your fortune ,\ndo you need instructions type (yes) or (no) \n")
 
-if a.lower == "yes":
+if a.lower() == "yes":
 
     print(''' 1. player rolls two six-sided dice and adds the numbers rolled together.
               2. On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12automatically loses, and play is over.
                  If a 4, 5, 6, 8, 9, or 10 are rolled on this first roll, that number becomes the 'point.'
               3. The player continues to roll the two dice again until one of two things happens:
                  either they roll the 'point' again, in which case they win; or they roll a 7, in which case they lose.''')
-elif a.lower == "no":
+elif a.lower() == "no":
     print("All the best player")
 
 def diceNumber():
@@ -30,7 +28,7 @@ def diceNumber():
 
 def twoDices(dices):
     dice1, dice2 = dices
-    print("player- the sum of numbers you have got in die 1 and die 2 are {} + {} = {}".format(dice1, dice2, sum(dices)))
+    print(f "player- the sum of numbers you have got in die 1 and die 2 are {dice1} and {dice2}.")
 
 value = diceNumber()
 twoDices(value)
@@ -63,11 +61,11 @@ while result == "continue your game please":
         result = "congratulations you won"
          
     elif sum_of_dices == 7:
-        result = "you lost,\n try again next time"
+        result = "you lost,/n try again next time"
  
 
 if result == "congratulations you won":
     print("congratulations,you won")
      
 else:
-    print("you lost, \ntry again next time")
+    print("you lost, /n try again next time")
